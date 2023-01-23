022dfe003ad86bf76782c4e257bb481046efbb5d
Ardit Podrimaj <ardit.podrimaj@outlook.com>
2023-01-23 23:24:12 +0100

    add app interfaces

---
+++ Application/Interfaces/IOrderItemRepo.cs
@@ -0,0 +1,10 @@
+﻿using OrderApi;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IOrderItemRepo
+    {
+        Task<List<OrderItem>> GetAllOrderItemsByOrderIds(List<int> orderIds);
+        Task<bool> InsertOrderItems(List<OrderItem> orderItems);
+    }
+}
---
+++ Application/Interfaces/IOrderRepo.cs
@@ -0,0 +1,12 @@
+﻿using OrderApi;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IOrderRepo
+    {
+        Task<List<Order>> GetAllOrders();
+        Task<Order> GetOrderById(int id);
+        Task<List<Order>> GetOrderByClientId(int clientId);
+        Task<bool> InsertOrder(Order order);
+    }
+}
