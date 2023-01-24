e263d68601e81e5d1aace5ffe4384483922f2ece
Ardit Podrimaj <ardit.podrimaj@outlook.com>
2023-01-24 10:10:30 +0100

    add domain entities

---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrderApi;
+
+public partial class Order
+{
+    public int Id { get; set; }
+
+    public int Status { get; set; }
+
+    public int ClientId { get; set; }
+
+    public DateTime CreateDateTime { get; set; }
+
+    public DateTime? UpdateDateTime { get; set; }
+}
