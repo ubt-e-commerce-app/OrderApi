4ffc88a34b75db579ce643f18fa2f644d86b9863
Ardit Podrimaj <ardit.podrimaj@outlook.com>
2023-01-24 10:16:32 +0100

    add db context and repos

--- Infrastructure.Database/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Infrastructure.Database
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Infrastructure.Database/DBContexts/OrderApiDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderApi;
+
+public partial class OrderApiDbContext : DbContext
+{
+    public OrderApiDbContext()
+    {
+    }
+
+    public OrderApiDbContext(DbContextOptions<OrderApiDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderItem> OrderItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=BCKLAP-BCHRML3;Database=OrderApiDb;User Id=BUCKAROO\\\\\\\\j.veselaj;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Order__3214EC070C451C50");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.CreateDateTime).HasColumnType("datetime");
+            entity.Property(e => e.UpdateDateTime).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__OrderIte__3214EC07E867D76A");
+
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
