4fab1e950ab48861e49b97f39906d1f636461986
Ardit Podrimaj <ardit.podrimaj@outlook.com>
2023-01-24 10:22:33 +0100

    add order api cons

---
+++ OrderApi/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderApi.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpGet]
+    [Route("api/order/{customerId}")]
+    public async Task<List<OrderDto>> GetOrdersByClientId(int customerId)
+    {
+        return await this._orderService.GetOrdersByClientId(customerId);
+    }
+
+
+    [HttpPost]
+    [Route("api/Order/Insert")]
+    public async Task<bool> InsertOrder([FromBody] OrderDto order)
+    {
+        return await this._orderService.InsertOrder(order);
+    }
+}
